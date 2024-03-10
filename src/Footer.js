e9c5f631f876e237f0efb69af5e3b2d0f6f31d99
pavanreddy20bc <pavanreddy20bcs183@iiitkottayam.ac.in>
2024-03-10 22:57:48 +0530

    search functionality

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p> this is a demo @airbnb clone</p>
+            
+        </div>
+    )
+}
+
+export default Footer
